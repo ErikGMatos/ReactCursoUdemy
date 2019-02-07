9b192bbd653c6208bf00fad5c7a537ae5f67126b
Erik Garces Matos <erik_senac@hotmail.com>
2019-02-07 12:56:34 -0200

    2 comiit

--- src/home/ListFooter.js
+++ src/home/ListFooter.js
@@ -1,8 +1,10 @@
-﻿import React, { Fragment } from 'react';
+﻿import React, { Fragment } from "react";
 
-
-const ListFooter = (props) => (
-    <Fragment><p>01/01/2019</p><p>R$ {props.total}</p></Fragment>
+const ListFooter = props => (
+    <Fragment>
+        <p>01/01/2019</p>
+        <p>R$ {props.total}</p>
+    </Fragment>
 );
 
 export default ListFooter;
